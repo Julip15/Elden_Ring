1c6a4f79058ca93ad21dd7b1bc9a19f40ef2a9e1
kw <wilsokia@oregonstate.edu>
2025-04-26 17:06:36 -0500

    DDL change

--- DDL.sql
+++ DDL.sql
@@ -3,6 +3,6 @@ Group: 49
 Name: Ethan Spear, Kia Wilson 
 OSUOID: _____,  wilsokia
 Course: CS340 Section 400
-Assignment: Project Step 2 Draft : Elden Ring 
+Assignment: Project Step 2 Draft 
 */
 
