8781d5c54d3c8c9d32bf5bccca3b96e9900226ff
kw <wilsokia@oregonstate.edu>
2025-05-07 01:05:39 -0500

    hbs pages for weapons, enemies, locations, regions, changes to main.hbs and app.js

--- ui/app.js
+++ ui/app.js
@@ -32,28 +32,92 @@ app.get('/', async function (req, res) {
     }
 });
 
-app.get('/bsg-people', async function (req, res) {
-    try {
-        // Create and execute our queries
-        // In query1, we use a JOIN clause to display the names of the homeworlds
-        const query1 = `SELECT bsg_people.id, bsg_people.fname, bsg_people.lname, \
-            bsg_planets.name AS 'homeworld', bsg_people.age FROM bsg_people \
-            LEFT JOIN bsg_planets ON bsg_people.homeworld = bsg_planets.id;`;
-        const query2 = 'SELECT * FROM bsg_planets;';
-        const [people] = await db.query(query1);
-        const [homeworlds] = await db.query(query2);
 
-        // Render the bsg-people.hbs file, and also send the renderer
-        //  an object that contains our bsg_people and bsg_homeworld information
-        res.render('bsg-people', { people: people, homeworlds: homeworlds });
+//Route handler for locations
+app.get('/locations', async (req, res) => {
+    try {
+        const [locations] = await db.query('SELECT * FROM Locations');
+        const [regions] = await db.query('SELECT region_id FROM Regions'); 
+
+        console.log('Regions:', regions);  
+
+        res.render('locations', { locations, regions });
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).send('Error fetching locations');
+    }
+});
+
+// Route handler for enemies
+app.get('/enemies', async function (req, res) {
+    try {
+        const query1 = `
+            SELECT Enemies.enemy_id, Enemies.name, IFNULL(Enemies.health, 'N/A') AS health, Enemies.is_boss,
+                   Enemies.weapon_id AS weapon_id, Enemies.location_id
+            FROM Enemies
+            LEFT JOIN Weapons ON Enemies.weapon_id = Weapons.weapon_id
+            LEFT JOIN Locations ON Enemies.location_id = Locations.location_id;
+        `;
+        const query2 = `SELECT * FROM Weapons;`;
+        const query3 = `SELECT * FROM Locations;`;
+
+        const [enemy] = await db.query(query1);
+        const [weapons] = await db.query(query2);
+        const [locations] = await db.query(query3);
+
+        res.render('enemies', {
+            enemies: enemy,
+            weapons: weapons,
+            locations: locations
+        });
+
     } catch (error) {
         console.error('Error executing queries:', error);
-        // Send a generic error message to the browser
         res.status(500).send(
             'An error occurred while executing the database queries.'
         );
     }
 });
+
+
+// Route handle for Weapons
+app.get('/weapons', async function (req, res) {
+    try {
+        const query1 = `
+            SELECT Weapons.weapon_id, Weapons.name, Weapons.damage, Weapon_Categories.category_id AS category_name
+            FROM Weapons
+            LEFT JOIN Weapon_Categories ON Weapons.category_id = Weapon_Categories.category_id;
+        `;
+        const query2 = `SELECT * FROM Weapon_Categories;`;
+
+        const [weapons] = await db.query(query1);
+        const [weapon_categories] = await db.query(query2);
+
+        res.render('weapons', { weapons, weapon_categories });
+
+    } catch (error) {
+        res.status(500).send(`<pre>${error.stack}</pre>`);
+    }
+});
+
+// Route handler for Regions 
+app.get('/regions', async function (req, res) {
+    try {
+        const query = `
+            SELECT region_id FROM Regions;
+        `;
+        
+        const [regions] = await db.query(query);
+
+        // Render the regions.hbs template and pass the regions data
+        res.render('regions', { regions });
+    } catch (error) {
+        res.status(500).send(`<pre>${error.stack}</pre>`);
+    }
+});
+
+
+
 
 // ########################################
 // ########## LISTENER
