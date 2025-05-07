fca76f46ce9f827a4eac5553936d2e31e1dbe1ca
Spear <speare@oregonstate.edu>
2025-05-07 12:58:42 -0700

    comments in dml

--- DML.sql
+++ DML.sql
@@ -7,39 +7,49 @@ Assignment: Elden Ring Project: Step 3 Draft
 */
 
 /* Players */
+/* Select for webpage */
 SELECT Players.player_id, Players.name, Players.class,
 Players.level, Players.death_count, Locations.name AS 'location' 
 FROM Players JOIN Locations ON Players.location_id = Locations.location_id;
 
+/* add new Player */
 INSERT INTO Players (name, class, level, death_count, location_id)
 VALUES (:nameinput, :classinput, :levelinput, :death_countinput, :location_idinput);
 
+/* delete Player */
 DELETE FROM Players WHERE player_id = :player_idinput;
 
+/* Select Player for update */
 SELECT player_id, name, class, level, death_count, location_id
 FROM Players
 WHERE player_id = :player_idinput;
 
+/* update Player */
 UPDATE Players
 SET name = :nameinput, class = :classinput, level = :levelinput, 
 death_count = :death_count, location_id = :location_idinput
 WHERE player_id = :player_idinput;
 
 /* Player_Weapons */
+/* Select for webpage */
 SELECT Players.name AS player, Weapons.name AS weapon, player_weapon_id
 FROM Player_Weapons
 JOIN Players On Players.player_id = Player_Weapons.player_id
 JOIN Weapons ON Weapons.weapon_id = Player_Weapons.weapon_id;
 
+/* add new Player_Weapon */
 INSERT INTO Player_Weapons (player_id, weapon_id)
 VALUES (:player_idinput, :weapon_idinput);
 
+/* delete Player_Weapon */
 DELETE FROM Player_Weapons WHERE player_weapon_id = :player_weapon_idinput;
 
+/* select for update Player_Weapon */
 SELECT player_id, weapon_id
 FROm Player_Weapons
 WHERE player_weapon_id = :player_weapon_idinput;
 
+/* update Player_Weapon */
 UPDATE Player_Weapons
 SET player_id = :player_idinput, weapon_id = :weapon_idinput
 WHERE player_weapon_id = :player_weapon_idinput;
