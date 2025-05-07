d0381351d2bb9cb1dc990fe404db6dc2b85aecf5
kw <wilsokia@oregonstate.edu>
2025-05-07 14:19:13 -0500

    manually mergerd app.js and dml.files

--- DML.sql
+++ DML.sql
@@ -5,15 +5,48 @@ OSUOID: speare,  wilsokia
 Course: CS340 Section 400
 Assignment: Elden Ring Project: Step 3 Draft
 */
-/*
-    DML queries
 
-1      # Citation for the following queries:
-2      # Date: 05/06/25
-3      # Based on: Project Step 3 Assignment Instructions; bsg_sample_data_manipulation_queries.sql
-4      # Source URL: https://canvas.oregonstate.edu/courses/1999601/assignments/10006387
-5
-*/
+/* Players */
+SELECT Players.player_id, Players.name, Players.class,
+Players.level, Players.death_count, Locations.name AS 'location' 
+FROM Players JOIN Locations ON Players.location_id = Locations.location_id;
+
+INSERT INTO Players (name, class, level, death_count, location_id)
+VALUES (:nameinput, :classinput, :levelinput, :death_countinput, :location_idinput);
+
+DELETE FROM Players WHERE player_id = :player_idinput;
+
+SELECT player_id, name, class, level, death_count, location_id
+FROM Players
+WHERE player_id = :player_idinput;
+
+UPDATE Players
+SET name = :nameinput, class = :classinput, level = :levelinput, 
+death_count = :death_count, location_id = :location_idinput
+WHERE player_id = :player_idinput;
+
+/* Player_Weapons */
+SELECT Players.name AS player, Weapons.name AS weapon, player_weapon_id
+FROM Player_Weapons
+JOIN Players On Players.player_id = Player_Weapons.player_id
+JOIN Weapons ON Weapons.weapon_id = Player_Weapons.weapon_id;
+
+INSERT INTO Player_Weapons (player_id, weapon_id)
+VALUES (:player_idinput, :weapon_idinput);
+
+DELETE FROM Player_Weapons WHERE player_weapon_id = :player_weapon_idinput;
+
+SELECT player_id, weapon_id
+FROm Player_Weapons
+WHERE player_weapon_id = :player_weapon_idinput;
+
+UPDATE Player_Weapons
+SET player_id = :player_idinput, weapon_id = :weapon_idinput
+WHERE player_weapon_id = :player_weapon_idinput;
+
+/* Weapon_categories */
+SELECT category_id
+FROM Weapon_categories
 
 --  populate weapons table with category names drop down.
 SELECT Weapons.weapon_id, Weapons.name, Weapons.damage, Weapon_Categories.category_id AS category_name
--- ui/app.js
+++ ui/app.js
@@ -2,13 +2,15 @@
 // ########## SETUP
 
 // Express
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-const PORT = 5709;
+const PORT = process.env.PORT;
 
 // Database
 const db = require('./database/db-connector');
@@ -32,6 +34,74 @@ app.get('/', async function (req, res) {
     }
 });
 
+app.get('/Players', async function (req, res) {
+    try {
+        // Create and execute our queries
+        // In query1, we use a JOIN clause to display the names of the homeworlds
+        const query1 = `SELECT Players.player_id, Players.name, Players.class,
+            Players.level, Players.death_count, Locations.name AS 'location' FROM Players
+            JOIN Locations ON Players.location_id = Locations.location_id;`;
+        const query2 = 'SELECT * FROM Locations;';
+        const [players] = await db.query(query1);
+        const [locations] = await db.query(query2);
+        //const [homeworlds] = await db.query(query2);
+
+        // Render the bsg-people.hbs file, and also send the renderer
+        //  an object that contains our bsg_people and bsg_homeworld information
+        res.render('Players', { players: players, locations: locations});
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+app.get('/Player_Weapons', async function (req, res) {
+    try {
+        // Create and execute our queries
+        // In query1, we use a JOIN clause to display the names of the homeworlds
+        const query1 = `SELECT Players.name AS 'player', Weapons.name AS 'weapon', player_weapon_id
+                        FROM Player_Weapons
+                        JOIN Players On Players.player_id = Player_Weapons.player_id
+                        JOIN Weapons ON Weapons.weapon_id = Player_Weapons.weapon_id;`;
+        const query2 = 'SELECT * FROM Players;';
+        const query3 = 'SELECT * FROM Weapons;';
+        const [inventory] = await db.query(query1);
+        const [players] = await db.query(query2);
+        const [weapons] = await db.query(query3);
+        //const [homeworlds] = await db.query(query2);
+
+        // Render the bsg-people.hbs file, and also send the renderer
+        //  an object that contains our bsg_people and bsg_homeworld information
+        res.render('Player_Weapons', { inventory: inventory, players: players, weapons: weapons});
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+app.get('/Weapon_Categories', async function (req, res) {
+    try {
+
+        const query1 = `SELECT category_id
+                        FROM Weapon_Categories;`;
+
+        const [categories] = await db.query(query1);
+
+        res.render('Weapon_Categories', { categories: categories});
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
 
 //Route handler for locations
 app.get('/locations', async function (req, res) {
@@ -115,6 +185,8 @@ app.get('/regions', async function (req, res) {
         res.status(500).send(`<pre>${error.stack}</pre>`);
     }
 });
+
+
 
 
 
