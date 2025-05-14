2554b8e277a41acbaa422a6e8867f482a5bf1cc8
Spear <speare@oregonstate.edu>
2025-05-14 12:59:32 -0700

    Website changes for Step 3 Final'

--- DML.sql
+++ DML.sql
@@ -8,9 +8,9 @@ Assignment: Elden Ring Project: Step 3 Draft
 
 /* Players */
 /* Select for webpage */
-SELECT Players.player_id, Players.name, Players.class,
-Players.level, Players.death_count, Locations.name AS 'location' 
-FROM Players JOIN Locations ON Players.location_id = Locations.location_id;
+SELECT Players.player_id AS 'Player ID', Players.name AS 'Name', Players.class AS 'Class',
+Players.level AS 'Level', Players.death_count AS 'Death Count', Locations.name AS 'Location' FROM Players
+JOIN Locations ON Players.location_id = Locations.location_id;
 
 /* add new Player */
 INSERT INTO Players (name, class, level, death_count, location_id)
@@ -32,7 +32,7 @@ WHERE player_id = :player_idinput;
 
 /* Player_Weapons */
 /* Select for webpage */
-SELECT Players.name AS player, Weapons.name AS weapon, player_weapon_id
+SELECT  player_weapon_id AS 'Inventory ID', Players.name AS 'Player', Weapons.name AS 'Weapon'
 FROM Player_Weapons
 JOIN Players On Players.player_id = Player_Weapons.player_id
 JOIN Weapons ON Weapons.weapon_id = Player_Weapons.weapon_id;
@@ -55,11 +55,11 @@ SET player_id = :player_idinput, weapon_id = :weapon_idinput
 WHERE player_weapon_id = :player_weapon_idinput;
 
 /* Weapon_categories */
-SELECT category_id
-FROM Weapon_categories
+SELECT category_id AS 'Category'
+FROM Weapon_Categories;
 
 --  populate weapons table with category names drop down.
-SELECT Weapons.weapon_id, Weapons.name, Weapons.damage, Weapon_Categories.category_id AS category_name
+SELECT Weapons.weapon_id AS 'Weapon ID', Weapons.name AS 'Name', Weapons.damage AS 'Damage', Weapon_Categories.category_id AS 'Category'
 FROM Weapons
 LEFT JOIN Weapon_Categories ON Weapons.category_id = Weapon_Categories.category_id;
 -- get all weapon categories to populate drop down when creating weapon 
@@ -67,10 +67,14 @@ SELECT * FROM Weapon_Categories;
 
 
 -- get all weapons and weapon attributes
-SELECT Enemies.enemy_id, Enemies.name, IFNULL(Enemies.health, 'N/A') AS health, Enemies.is_boss,
-       Enemies.weapon_id AS weapon_id, Enemies.location_id
+SELECT Enemies.enemy_id AS 'Enemy ID', Enemies.name AS 'Name', IFNULL(Enemies.health, 'N/A') AS Health,             
+CASE
+    WHEN Enemies.is_boss = 1 THEN 'YES'
+    WHEN Enemies.is_boss = 0 THEN 'NO'
+END AS 'Is Boss',
+Weapons.name as 'Weapon', Locations.name as 'Location'
 FROM Enemies
-LEFT JOIN Weapons ON Enemies.weapon_id = Weapons.weapon_id,
+LEFT JOIN Weapons ON Enemies.weapon_id = Weapons.weapon_id
 LEFT JOIN Locations ON Enemies.location_id = Locations.location_id;
 -- get all weapons to populate drop down for weapons when creating enenmy
 SELECT * FROM Weapons;
@@ -79,12 +83,12 @@ SELECT * FROM Locations;
 
 
 -- get all locations and location attributes
-SELECT * FROM Locations;
+SELECT location_id AS 'Location ID', name AS 'Name', region_id AS 'Region' FROM Locations;
 -- get region_id to populate drop down for creating a location 
 SELECT region_id from Regions;
 
 -- selct all regions
-SELECT region_id FROM Regions;
+SELECT region_id AS 'Region' FROM Regions;
 
 -- add new weapon
 INSERT INTO Weapons(name, damage, category_id) VALUES(:nameInput, :damageInput, :category_id_from_dropdown_Input);
