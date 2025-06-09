86d911943bf3359fab197e48e138576594ce5b56
Spear-e <speare@oregonstate.edu>
2025-06-09 14:47:00 -0700

    Update DML.sql

--- DML.sql
+++ DML.sql
@@ -1,9 +1,9 @@
 /*
-Group: 49
-Name: Ethan Spear, Kia Wilson 
-OSUOID: speare,  wilsokia
-Course: CS340 Section 400
-Assignment: Elden Ring Project: Step 3 Draft
+Group:  49
+Name:  Ethan Spear, Kia Wilson 
+OSUOID:  speare,  wilsokia
+Course:  CS340 Section 400
+Assignment:  Elden Ring Project  Step 4 Draft
 */
 
 /* Players */
@@ -14,21 +14,21 @@ JOIN Locations ON Players.location_id = Locations.location_id;
 
 /* add new Player */
 INSERT INTO Players (name, class, level, death_count, location_id)
-VALUES (:nameinput, :classinput, :levelinput, :death_countinput, :location_idinput);
+VALUES ( nameinput,  classinput,  levelinput,  death_countinput,  location_idinput);
 
 /* delete Player */
-DELETE FROM Players WHERE player_id = :player_idinput;
+DELETE FROM Players WHERE player_id = pid;
 
 /* Select Player for update */
 SELECT player_id, name, class, level, death_count, location_id
 FROM Players
-WHERE player_id = :player_idinput;
+WHERE player_id =  player_idinput;
 
 /* update Player */
 UPDATE Players
-SET name = :nameinput, class = :classinput, level = :levelinput, 
-death_count = :death_count, location_id = :location_idinput
-WHERE player_id = :player_idinput;
+SET name =  nameinput, class =  classinput, level =  levelinput, 
+death_count =  death_count, location_id =  location_idinput
+WHERE player_id =  player_idinput;
 
 /* Player_Weapons */
 /* Select for webpage */
@@ -39,20 +39,20 @@ JOIN Weapons ON Weapons.weapon_id = Player_Weapons.weapon_id;
 
 /* add new Player_Weapon */
 INSERT INTO Player_Weapons (player_id, weapon_id)
-VALUES (:player_idinput, :weapon_idinput);
+VALUES ( player_idinput,  weapon_idinput);
 
 /* delete Player_Weapon */
-DELETE FROM Player_Weapons WHERE player_weapon_id = :player_weapon_idinput;
+DELETE FROM Player_Weapons WHERE player_weapon_id =  player_weapon_idinput;
 
 /* select for update Player_Weapon */
 SELECT player_id, weapon_id
 FROm Player_Weapons
-WHERE player_weapon_id = :player_weapon_idinput;
+WHERE player_weapon_id =  player_weapon_idinput;
 
 /* update Player_Weapon */
 UPDATE Player_Weapons
-SET player_id = :player_idinput, weapon_id = :weapon_idinput
-WHERE player_weapon_id = :player_weapon_idinput;
+SET player_id =  player_idinput, weapon_id =  weapon_idinput
+WHERE player_weapon_id =  player_weapon_idinput;
 
 /* Weapon_categories */
 SELECT category_id AS 'Category'
@@ -91,12 +91,12 @@ SELECT region_id from Regions;
 SELECT region_id AS 'Region' FROM Regions;
 
 -- add new weapon
-INSERT INTO Weapons(name, damage, category_id) VALUES(:nameInput, :damageInput, :category_id_from_dropdown_Input);
+INSERT INTO Weapons(name, damage, category_id) VALUES( nameInput,  damageInput,  category_id_from_dropdown_Input);
 
 -- add a new enemy
 INSERT INTO Enemies(name, health, is_boss, weapon_id, location_id)
-VALUES(:nameInput, :healthInput, :is_bossInput, :weapon_id_from_dropdown_Input, :location_id_from_dropdown_Input);
+VALUES( nameInput,  healthInput,  is_bossInput,  weapon_id_from_dropdown_Input,  location_id_from_dropdown_Input);
 
 -- add a new location
-INSERT INTO Locations(name, region_id) VALUES(:nameInput, :region_id_from_dropdown_Input);
+INSERT INTO Locations(name, region_id) VALUES( nameInput,  region_id_from_dropdown_Input);
 
